219afcfc001ae57bfabe71c123bdb1d799c72dbc
gaganalexa <202310537@fit.edu.ph>
2025-05-13 19:42:17 +0800

    feat(goals): adding html and php components

--- pages/hobbies/index.php
+++ pages/hobbies/index.php
@@ -13,6 +13,34 @@
             <a href="../favorites/index.php" class="btn">My Favorites</a>
             <a href="../goals/index.php" class="btn">My Goals</a>
         </div>
+        <div class="hero-section">
+                <h1>Things I Enjoy</h1>
+                <p>Your hobby is a great way to improve your skills while enjoying!</p>
+        </div>
+        <div class="content">
+            <h3>FOR MY FREE TIME</h3>
+            <p>The following is what I do:</p>
+            <div class="line1"></div>
+            <div class="hobby-grid">
+                <?php
+                //Declarations
+                $hobbies = [
+                    ['title' => 'CODING', 'desc' => 'Python, PHP, Java'],
+                    ['title' => 'GAMING', 'desc' => 'Mobile Legends'],
+                    ['title' => 'COOKING', 'desc' => 'Complex Foods'],
+                    ['title' => 'GARDENING', 'desc' => 'Outdoor and Indoor']
+                ];
 
+                //Looping
+                foreach ($hobbies as $hobby){
+                    echo "<div class='hobby-box'>";
+                    echo "<h4>{$hobby['title']}</h4>";
+                    echo "<p>{$hobby['desc']}</p>";
+                    echo "</div>";
+                }
+                ?>
+            <div class="line1"></div>
+            </div>
+        </div>
 </body>
 </html>

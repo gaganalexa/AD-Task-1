c21195a166f122afc8cacf6117c28646e84dd582
gaganalexa <202310537@fit.edu.ph>
2025-05-13 18:49:48 +0800

    feat(landing): adding my html structure

--- index.php
+++ index.php
@@ -7,7 +7,22 @@
     <title>Document</title>
 </head>
 <body>
-
+    <div class="container">
+            <div class="left">
+                <h1>
+                    <span class="highlight">HELLO, I AM</span><br> ALEXA MHEL<br> V. GAGAN
+                </h1>
+            <div class="line"></div>
+            
+            </div>
+            <div class="center"></div>
+            <div class="right">
+                <a href="index.php" class="btn">Introduction</a>
+                <a href="pages/hobbies/index.php" class="btn">My Hobbies</a>
+                <a href="pages/favorites/index.php" class="btn">My Favorites</a>
+                <a href="pages/goals/index.php" class="btn">My Goals</a>
+            </div>
+        </div>
 
 </body>
 </html>

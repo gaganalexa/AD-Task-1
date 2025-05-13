542edfa7fc53115b5a7b67559d0c40f4c7a22bb9
gaganalexa <202310537@fit.edu.ph>
2025-05-13 19:04:08 +0800

    feat(favorites): adding html and php codes

--- pages/favorites/index.php
+++ pages/favorites/index.php
@@ -13,5 +13,39 @@
             <a href="index.php" class="btn">My Favorites</a>
             <a href="../goals/index.php" class="btn">My Goals</a>
         </div>
+        <div class="hero-section">
+            <h1>Things I Like</h1>
+            <p>We all have our favorite things. May it be songs, food, color, etc.</p>
+        </div>
+        <div class="content">
+            <h3>FAVORITE LISTS</h3>
+            <p>The following are my favorites:</p>
+            <div class="line1"></div>
+            <div class="hobby-grid">
+                <?php
+                //Declarations
+                $favorites = [
+                    ['title' => 'FOOD', 'desc' => 'Adobo & Pastil'],
+                    ['title' => 'COLOR', 'desc' => 'Any Shade of Blue'],
+                    ['title' => 'BOOK', 'desc' => 'Animal Farm'],
+                    ['title' => 'PLACE', 'desc' => 'Quezon Province'],
+                    ['title' => 'MOVIE', 'desc' => 'Brooklyn Nine-Nine'],
+                    ['title' => 'FLAVOR', 'desc' => 'Ube & Matcha'],
+                    ['title' => 'DAY', 'desc' => 'Saturday'],
+                    ['title' => 'SONG', 'desc' => 'Irish - The Goo Goo Dolls']
+                ];
+
+                //Looping
+                foreach ($favorites as $items){
+                    echo "<div class='hobby-box'>";
+                    echo "<h4>{$items['title']}</h4>";
+                    echo "<p>{$items['desc']}</p>";
+                    echo "</div>";
+                }
+                ?>
+            <div class="line1"></div>
+            </div>
+        </div>
+
 </body>
 </html>
